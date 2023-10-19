f52cc35b0543d2ab89c3bd5c1de679be5126e187
toby4all <toby4all@gmail.com>
2023-10-20 00:33:56 +0200

    docker compose modifying 7

--- db_connector.py
+++ db_connector.py
@@ -3,14 +3,17 @@ import pymysql
 def add_user(user_id, username):
     schema_name = 'mydb'
     # Establishing a connection to the MySQL container within the Docker Compose network
-    conn = pymysql.connect(host='mysql', port=3306, user='user', password='password', db=schema_name)
+    conn = pymysql.connect(host='db', port=3307, user='user', password='password', db=schema_name)
     conn.autocommit(True)
 
     # Getting a cursor from the database connection
     cursor = conn.cursor()
 
-    # Inserting data into the table
-    cursor.execute(f"INSERT into {schema_name}.users (user_id, name) VALUES ({user_id}, '{username}')")
+    try:
+        # Inserting data into the table
+        cursor.execute(f"INSERT into {schema_name}.users (user_id, name) VALUES (%s, %s)", (user_id, username))
+    except pymysql.MySQLError as e:
+        print(f"Error: {e}")
 
     cursor.close()
     conn.close()
@@ -18,19 +21,22 @@ def add_user(user_id, username):
 def get_user(user_id):
     schema_name = 'mydb'
     # Establishing a connection to the MySQL container within the Docker Compose network
-    conn = pymysql.connect(host='mysql', port=3306, user='user', passwd='password', db=schema_name)
+    conn = pymysql.connect(host='db', port=3307, user='user', password='password', db=schema_name)
     conn.autocommit(True)
 
     # Getting a cursor from the database connection
     cursor = conn.cursor()
 
-    # Select data from the table
-    cursor.execute(f"SELECT name FROM {schema_name}.users WHERE user_id =({user_id}) ")
-    result = cursor.fetchone()
-    if result:
-        return result[0]
-    else:
-        return None
+    try:
+        # Select data from the table
+        cursor.execute(f"SELECT name FROM {schema_name}.users WHERE user_id = %s", (user_id,))
+        result = cursor.fetchone()
+        if result:
+            return result[0]
+        else:
+            return None
+    except pymysql.MySQLError as e:
+        print(f"Error: {e}")
 
     cursor.close()
     conn.close()
@@ -38,14 +44,18 @@ def get_user(user_id):
 def update_user(user_id, username):
     schema_name = 'mydb'
     # Establishing a connection to the MySQL container within the Docker Compose network
-    conn = pymysql.connect(host='mysql', port=3306, user='user', passwd='password', db=schema_name)
+    conn = pymysql.connect(host='db', port=3307, user='user', password='password', db=schema_name)
     conn.autocommit(True)
 
     # Getting a cursor from the database connection
     cursor = conn.cursor()
-    sql = f"UPDATE {schema_name}.users SET name = %s WHERE user_id = %s"
-    val = (username, user_id)
-    cursor.execute(sql, val)
+
+    try:
+        sql = f"UPDATE {schema_name}.users SET name = %s WHERE user_id = %s"
+        val = (username, user_id)
+        cursor.execute(sql, val)
+    except pymysql.MySQLError as e:
+        print(f"Error: {e}")
 
     cursor.close()
     conn.close()
@@ -53,12 +63,17 @@ def update_user(user_id, username):
 def delete_user(user_id):
     schema_name = 'mydb'
     # Establishing a connection to the MySQL container within the Docker Compose network
-    conn = pymysql.connect(host='mysql', port=3306, user='user', passwd='password', db=schema_name)
+    conn = pymysql.connect(host='db', port=3307, user='user', password='password', db=schema_name)
     conn.autocommit(True)
 
     # Getting a cursor from the database connection
     cursor = conn.cursor()
-    cursor.execute(f"DELETE FROM {schema_name}.users WHERE user_id = '{user_id}'")
+
+    try:
+        cursor.execute(f"DELETE FROM {schema_name}.users WHERE user_id = %s", (user_id,))
+    except pymysql.MySQLError as e:
+        print(f"Error: {e}")
 
     cursor.close()
     conn.close()
+
