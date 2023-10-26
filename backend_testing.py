08b5be685e1c1060ef8c81747233bd5d00e5b39d
toby4all <toby4all@gmail.com>
2023-10-26 18:05:42 +0200

    docker compose modifying 10

--- backend_testing.py
+++ backend_testing.py
@@ -1,44 +1,43 @@
 import requests
 import pymysql
 
-# request to post new data to database with usr_name 'John'
-res = requests.post('http://127.0.0.1:5000/users/4', json={"name": "Anabella"})
+# request to post new data to the database with user_id '4'
+res = requests.post('http://localhost:5000/users/4', json={"name": "Anabella"})
 if res.status_code == 200:
     print(res.json())
 
-# request to get data from database with usr_id '3'
-response = requests.get('http://127.0.0.1:5000/users/4')
+# request to get data from the database with user_id '4'
+response = requests.get('http://localhost:5000/users/4')
 if response.ok:
     print(response.status_code)
     print(response.json())
 
-# query database to make sure username is stored under id:'3'
-#
+# query the database to make sure the username is stored under id: '4'
 schema_name = "mydb"
 
-# Establishing a connection to DB
-conn = pymysql.connect(host='127.0.0.1', port=3306, user='user', passwd='password', db=schema_name)
-# Getting a cursor from Database
+# Establishing a connection to the DB
+conn = pymysql.connect(host='localhost', port=3306, user='user', passwd='password', db=schema_name)
+# Getting a cursor from the Database
 cursor = conn.cursor()
 
-# Getting all data from table “users”
-cursor.execute(f"SELECT * FROM {schema_name}.users WHERE user_id ='4' ")
+# Getting all data from the table "users"
+cursor.execute(f"SELECT * FROM {schema_name}.users WHERE user_id = '4'")
 
-# Iterating table and printing all users
+# Iterating the table and printing all users
 for row in cursor:
     print(row)
 
 cursor.close()
 conn.close()
 
-# request to update user_name in database to new data "George"
-response = requests.put('http://127.0.0.1:5000/users/2', json={"name": "George"})
+# request to update user_name in the database to new data "George"
+response = requests.put('http://localhost:5000/users/2', json={"name": "George"})
 if response.ok:
     print(response.status_code)
     print(response.json())
 
 # request to delete user_id "2"
-response = requests.delete('http://127.0.0.1:5000/users/2')
-if response.status_code == 2:
+response = requests.delete('http://localhost:5000/users/2')
+if response.status_code == 200:
     print(response.json())
     print(response.status_code)
